dfc372ab8f4eabd9067bdd26540ee6d73716fcf0
yeong <133121353+ssyyeong@users.noreply.github.com>
2024-11-29 18:39:53 +0900

    Update index.web.tsx

--- index.web.tsx
+++ index.web.tsx
@@ -2,7 +2,6 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import {Platform} from 'react-native';
-import React from 'react';
 
 if (Platform.OS === 'web') {
   AppRegistry.registerComponent(appName, () => App);
