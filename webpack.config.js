4a81a3582c8c466d329c237ccf5927838b27d90b
yeong <133121353+ssyyeong@users.noreply.github.com>
2024-11-30 14:22:56 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -40,20 +40,6 @@ module.exports = {
       },
     ],
   },
-  module: {
-    rules: [
-      {
-        test: /\.(t|j)sx?$/,
-        exclude: /node_modules\/(?!()\/).*/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react'],
-          },
-        },
-      },
-    ],
-  },
   plugins: [HTMLWebpackPluginConfig],
   devServer: {
     static: {
